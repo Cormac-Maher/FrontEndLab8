57fca638a3dc93d622e53d2d4b6350bd82554dfa
unknown <G00463317@atu.ie>
2026-04-17 12:12:49 +0000

    Lab 8

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [RouterLink, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel],
 })
 export class HomePage {
-  constructor() {}
+  status:string="";
+  constructor(private storage:Storage) {}   // stroage import from injected
+
+  // retrive status from storage
+  async ionViewWillEnter(){
+    await this.storage.create();
+    this.status = await this.storage.get('myStatus')
+  }
 }
---
+++ src/app/status/status.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRadioGroup, IonList, IonItem, IonLabel, IonRadio, IonButton } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+// imported storage 
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonRadioGroup, IonList, IonItem, IonLabel, IonRadio, IonButton]
+})
+export class StatusPage implements OnInit {
+  status:string= "";
+  constructor(private storage:Storage) { }
+
+// creates local storage and saves status
+  async saveStatus(){
+    console.log(this.status);
+    await this.storage.create();
+    await this.storage.set('myStatus', this.status);
+  }
+  ngOnInit() {
+  }
+
+  // retrieves satus from local storage
+  async ionViewWillEnter(){
+    await this.storage.create();
+    this.status = await this.storage.get('myStatus');
+  }
+}
